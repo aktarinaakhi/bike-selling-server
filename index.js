cf95ce1fef516ca7642d993d32770ca052a6c4c4
aktarinaakhi <aktarinacsehstu@gmail.com>
2021-11-10 21:11:40 +0600

    added products post api

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9m4lb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connect successful')
+        const database = client.db("moto-bike");
+        const productsCollection = database.collection("products");
+        const ordersCollection = database.collection("orders");
+        const usersCollection = database.collection("users");
+
+
+        // all products GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({})
+            const products = await cursor.toArray();
+            res.json(products);
+        })
+
+        // products POST api
+
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            console.log(product)
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+        });
+
+        //orders post api
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const result = await ordersCollection.insertOne(orders);
+            res.json(result);
+        });
+
+        //all orders get api
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({})
+            const orders = await cursor.toArray();
+            res.json(orders);
+        });
+
+
+        //users post api
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            const result = await usersCollection.insertOne(users);
+            res.json(result);
+        });
+
+        //users get api
+
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({})
+            const orders = await cursor.toArray();
+            res.json(orders);
+        });
+
+
+        //Delete  Api 
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        //update orders status
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedStatus = req.body;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    status: updatedStatus.status
+                },
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('moto bike server is running');
+});
+
+app.listen(port, () => {
+    console.log('moto bike server server running', port);
+})
